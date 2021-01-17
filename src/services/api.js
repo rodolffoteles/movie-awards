255fe34aa874618c33c7bc44ce3e980bb351b009
Rodolffo Teles <rodolffoteles@hotmail.com>
2021-01-17 15:45:12 -0300

    Use HTTPS to fetch API

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import qs from 'qs';
 import camelCaseKeys from 'camelcase-keys';
 
 const api = axios.create({
-  baseURL: 'http://www.omdbapi.com/',
+  baseURL: 'https://www.omdbapi.com/',
   paramsSerializer: params => {
     const serializedParams = qs.stringify(params, { arrayFormat: 'repeat' });
     return `${serializedParams}&movie=movie&&apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
